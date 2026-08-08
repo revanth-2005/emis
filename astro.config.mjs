76a22644ef824ed1f236f9f9d416c96388ec2b1a
revanth-2005 <revanthrevanthcr7@gmail.com>
2026-08-08 17:14:54 +0530

    fd

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import node from '@astrojs/node';
 
 export default defineConfig({
   site: 'https://www.ermisglobal.com',
-  output: 'hybrid',
+  output: 'server',
   adapter: node({
     mode: 'standalone',
   }),
